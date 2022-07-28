3cdc8793f005e7454b53debff317cd993d2b9797
jasonh0509 <102926529+jasonh0509@users.noreply.github.com>
2022-07-28 00:21:34 -0400

    2020 graph added

--- static graph minority.R
+++ static graph minority.R
@@ -13,17 +13,26 @@ totaldata<-rbind(NA2010,NA2015,NA2020)
 
 sum(is.na(NA2010$Value.Count))
 
-graph2010<-subset(NA2010,select = c(Place,Year,Geoid,Rate.Denom,Value.Count,Value.Rate))
-graph2010$FIPS=graph2010$Geoid-37000
+graphtotal<-subset(totaldata,select = c(Place,Year,Geoid,Rate.Denom,Value.Count,Value.Rate))
+graphtotal$FIPS=graphtotal$Geoid-37000
 
 map=readOGR(dsn='Shapes',layer='cb_2014_us_county_500k')
 
 ncMap=fortify(map[oh.map$STATEFP=='37',],region='COUNTYFP')
 ncMap$id2=as.numeric(ncMap$id)
-ncMap.merge=merge(ncMap,graph2010,by.x="id2",by.y="FIPS")
+ncMap.merge=merge(ncMap,graphtotal,by.x="id2",by.y="FIPS")
+
+#2010 data 
+mapping2010<-ncMap.merge[which(ncMap.merge$Year==2010),]
+
+#2015 data
+mapping2015<-ncMap.merge[which(ncMap.merge$Year==2015),]
+
+#2020 data
+mapping2020<-ncMap.merge[which(ncMap.merge$Year==2020),]
 
 plot2010<-ggplot()+
-  geom_polygon(data=ncMap.merge,aes(x=long,y=lat,group=group,fill=Value.Rate),color='black',alpha=.8,size=.3)+
+  geom_polygon(data=mapping2010,aes(x=long,y=lat,group=group,fill=Value.Rate),color='black',alpha=.8,size=.3)+
   #scale_fill_gradient2(name="",limits=limits,low="navy", mid="white", high="red")+
   scale_fill_gradient(low="#FFFFFF",high = "#8b0000",)+
   coord_map()+
@@ -32,12 +41,17 @@ plot2010<-ggplot()+
 plot2010
 
 plot2015<-ggplot()+
-  geom_polygon(data=ncMap.merge,aes(x=long,y=lat,group=group,fill=Value.Rate),color='black',alpha=.8,size=.3)+
+  geom_polygon(data=mapping2015,aes(x=long,y=lat,group=group,fill=Value.Rate),color='black',alpha=.8,size=.3)+
   #scale_fill_gradient2(name="",limits=limits,low="navy", mid="white", high="red")+
   scale_fill_gradient(low="#FFFFFF",high = "#8b0000",)+
   coord_map()+
   theme_nothing(base_size=12, legend=T)+
-  ggtitle("AI/AN Rate Map 2010")+theme(plot.title = element_text(hjust = 0.5,size = rel(2.25)),legend.text=element_text(size=rel(2)),legend.key.size=unit(2,"line"))
-plot2010
+  ggtitle("AI/AN Rate Map 2015")+theme(plot.title = element_text(hjust = 0.5,size = rel(2.25)),legend.text=element_text(size=rel(2)),legend.key.size=unit(2,"line"))
+plot2015
 
-try<-DataForJason
+
+
+
+grid.arrange(plot2010,plot2015,plot2020,nrow=3)
+
+
