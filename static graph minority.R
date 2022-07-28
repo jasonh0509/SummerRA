a61c2b454b76008f22857a0d713ec2dceb991a5f
jasonh0509 <102926529+jasonh0509@users.noreply.github.com>
2022-07-28 03:02:35 -0400

    loading all dataset

--- static graph minority.R
+++ static graph minority.R
@@ -1,8 +1,15 @@
 
-library(ggplot2)
-library(broom)
-library(dplyr)
-library(tidyverse)
+library('rgeos')
+library('spdep')
+library('rgdal')
+library('maptools')
+library('shapefiles')
+library('ggmap')
+library('ggpubr')
+library('ggplot2')
+library('gridExtra')
+library('sf')
+library('broom')
 
 
 NA2010<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/Minority2010.csv")
@@ -16,14 +23,16 @@ sum(is.na(NA2010$Value.Count))
 graphtotal<-subset(totaldata,select = c(Place,Year,Geoid,Rate.Denom,Value.Count,Value.Rate))
 graphtotal$FIPS=graphtotal$Geoid-37000
 
-map=readOGR(dsn='Shapes',layer='cb_2014_us_county_500k')
+mapG=readOGR(dsn='Shapes',layer='cb_2014_us_county_500k')
 
-ncMap=fortify(map[oh.map$STATEFP=='37',],region='COUNTYFP')
+ncMap=fortify(mapG[mapG$STATEFP=='37',],region='COUNTYFP')
 ncMap$id2=as.numeric(ncMap$id)
 ncMap.merge=merge(ncMap,graphtotal,by.x="id2",by.y="FIPS")
 
 #2010 data 
 mapping2010<-ncMap.merge[which(ncMap.merge$Year==2010),]
+mapping2010<-mapping2010%>%
+  mutate(RateCal=(mapping2010$Value.Rate)/(mapping2010$Rate.Denom))
 
 #2015 data
 mapping2015<-ncMap.merge[which(ncMap.merge$Year==2015),]
@@ -31,14 +40,18 @@ mapping2015<-ncMap.merge[which(ncMap.merge$Year==2015),]
 #2020 data
 mapping2020<-ncMap.merge[which(ncMap.merge$Year==2020),]
 
+limits = c(min(NA2010$Value.Rate),max(NA2010$Value.Rate))
+
+
 plot2010<-ggplot()+
   geom_polygon(data=mapping2010,aes(x=long,y=lat,group=group,fill=Value.Rate),color='black',alpha=.8,size=.3)+
-  #scale_fill_gradient2(name="",limits=limits,low="navy", mid="white", high="red")+
-  scale_fill_gradient(low="#FFFFFF",high = "#8b0000",)+
+  scale_fill_gradient2(name="",limits=limits,low="navy", mid="white", high="red")+
+  #scale_fill_gradient(low="#FFFFFF",high = "#8b0000",)+
   coord_map()+
   theme_nothing(base_size=12, legend=T)+
   ggtitle("AI/AN Rate Map 2010")+theme(plot.title = element_text(hjust = 0.5,size = rel(2.25)),legend.text=element_text(size=rel(2)),legend.key.size=unit(2,"line"))
 plot2010
+
 
 plot2015<-ggplot()+
   geom_polygon(data=mapping2015,aes(x=long,y=lat,group=group,fill=Value.Rate),color='black',alpha=.8,size=.3)+
@@ -50,8 +63,40 @@ plot2015<-ggplot()+
 plot2015
 
 
+plot2020<-ggplot()+
+  geom_polygon(data=mapping2020,aes(x=long,y=lat,group=group,fill=Value.Rate),color='black',alpha=.8,size=.3)+
+  #scale_fill_gradient2(name="",limits=limits,low="navy", mid="white", high="red")+
+  scale_fill_gradient(low="#FFFFFF",high = "#8b0000",)+
+  coord_map()+
+  theme_nothing(base_size=12, legend=T)+
+  ggtitle("AI/AN Rate Map 2020")+theme(plot.title = element_text(hjust = 0.5,size = rel(2.25)),legend.text=element_text(size=rel(2)),legend.key.size=unit(2,"line"))
+plot2020
+
 
 
 grid.arrange(plot2010,plot2015,plot2020,nrow=3)
 
+##Asian Data
+Asian2010<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/Asian2010.csv")
+Asian2015<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/Asian2015.csv")
+Asian2020<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/Asian2020.csv")
+
+##Hispanic Data
+
+Hisp2010<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/HISP2010.csv")
+Hisp2015<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/HISP2015.csv")
+Hisp2020<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/HISP2020.csv")
+
+
+##Black Data
+
+Black2010<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/AA2010.csv")
+Black2015<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/AA2015.csv")
+Black2020<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/AA2020.csv")
+
+##White Data
+
+White2010<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/White2010.csv")
+White2015<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/White2015.csv")
+White2020<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/White2020.csv")
 
