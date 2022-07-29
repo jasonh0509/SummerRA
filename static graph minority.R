6763fc7b59031800cb363dd527054b579de78afb
jasonh0509 <jueshenhou@gmail.com>
2022-07-29 14:40:09 -0400

    adjusted scale succeed

--- static graph minority.R
+++ static graph minority.R
@@ -88,7 +88,7 @@ totaldataAsian<-rbind(Asian2010,Asian2015,Asian2020)
 totaldataAsian<-totaldataAsian%>%
   mutate(Rate.Denom=gsub(",","",Rate.Denom),
          Rate.Denom=as.numeric(Rate.Denom),
-         Value.Rate=((Value.Count)/(Rate.Denom))*1000)
+         Value.Rate=log(Value.Rate))
 
 #Asian Merge and map file
 
@@ -101,7 +101,7 @@ Asianmapping2010<-ncMap.mergeAsian[which(ncMap.mergeAsian$Year==2010),]
 Asianmapping2015<-ncMap.mergeAsian[which(ncMap.mergeAsian$Year==2015),]
 Asianmapping2020<-ncMap.mergeAsian[which(ncMap.mergeAsian$Year==2020),]
 
-
+#Additional
 b<-c(0,1.5,3)
 limits = c(min(Asian2010$Value.Rate),max(Asian2010$Value.Rate))
 
@@ -110,7 +110,8 @@ limits = c(min(Asian2010$Value.Rate),max(Asian2010$Value.Rate))
 Asianplot2010<-ggplot()+
   geom_polygon(data=Asianmapping2010,aes(x=long,y=lat,group=group,fill=Value.Rate),color='black',alpha=.8,size=.3)+
   #scale_fill_gradient2(name="",limits=limits,low="navy", mid="white", high="red")+
-  scale_fill_gradient(low="#FFFFFF",high = "#8b0000")+
+  scale_fill_gradient(low="#FFFFFF",high = "#8b0000",limits = c(0,6),
+                      breaks = c(0,3,6))+
   coord_map()+
   theme_nothing(base_size=12, legend=T)+
   ggtitle("Asian Rate Map 2010")+theme(plot.title = element_text(hjust = 0.5,size = rel(2.25)),legend.text=element_text(size=rel(2)),legend.key.size=unit(2,"line"))
@@ -119,7 +120,8 @@ Asianplot2010
 Asianplot2015<-ggplot()+
   geom_polygon(data=Asianmapping2015,aes(x=long,y=lat,group=group,fill=Value.Rate),color='black',alpha=.8,size=.3)+
   #scale_fill_gradient2(name="",limits=limits,low="navy", mid="white", high="red")+
-  scale_fill_gradient(low="#FFFFFF",high = "#8b0000")+
+  scale_fill_gradient(low="#FFFFFF",high = "#8b0000",limits = c(0,6),
+                       breaks = c(0,3,6))+
   coord_map()+
   theme_nothing(base_size=12, legend=T)+
   ggtitle("Asian Rate Map 2015")+theme(plot.title = element_text(hjust = 0.5,size = rel(2.25)),legend.text=element_text(size=rel(2)),legend.key.size=unit(2,"line"))
@@ -128,13 +130,14 @@ Asianplot2015
 Asianplot2020<-ggplot()+
   geom_polygon(data=Asianmapping2020,aes(x=long,y=lat,group=group,fill=Value.Rate),color='black',alpha=.8,size=.3)+
   #scale_fill_gradient2(name="",limits=limits,low="navy", mid="white", high="red")+
-  scale_fill_gradient(low="#FFFFFF",high = "#8b0000")+
+  scale_fill_gradient(low="#FFFFFF",high = "#8b0000",limits = c(0,6),
+                       breaks = c(0,3,6))+
   coord_map()+
   theme_nothing(base_size=12, legend=T)+
   ggtitle("Asian Rate Map 2020")+theme(plot.title = element_text(hjust = 0.5,size = rel(2.5)),legend.text=element_text(size=rel(2)),legend.key.size=unit(2,"line"))
 Asianplot2020
 
-AsianGraph<-grid.arrange(Asianplot2010,Asianplot2015,Asianplot2020,nrow=3)
+AsianGraph<-grid.arrange(Asianplot2010,Asianplot2015,Asianplot2020,ncol=3)
 
 ##Hispanic Data
 
@@ -142,6 +145,53 @@ Hisp2010<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/H
 Hisp2015<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/HISP2015.csv")
 Hisp2020<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/HISP2020.csv")
 
+totaldataHisp<-rbind(Hisp2010,Hisp2015,Hisp2020)
+#Rate.de<-subset(totaldataAsian,select = Rate.Denom)
+totaldataHisp<-totaldataHisp%>%
+  mutate(Rate.Denom=gsub(",","",Rate.Denom),
+         Rate.Denom=as.numeric(Rate.Denom),
+         Value.Rate=log(Value.Rate))
+
+graphtotalHisp<-subset(totaldataHisp,select = c(Place,Year,Geoid,Rate.Denom,Value.Count,Value.Rate))
+graphtotalHisp$FIPS=graphtotalHisp$Geoid-37000
+
+ncMap.mergeHisp<-merge(ncMap,graphtotalHisp,by.x="id2",by.y="FIPS")
+
+Hispmapping2010<-ncMap.mergeHisp[which(ncMap.mergeHisp$Year==2010),]
+Hispmapping2015<-ncMap.mergeHisp[which(ncMap.mergeHisp$Year==2015),]
+Hispmapping2020<-ncMap.mergeHisp[which(ncMap.mergeHisp$Year==2020),]
+
+Hispplot2010<-ggplot()+
+  geom_polygon(data=Hispmapping2010,aes(x=long,y=lat,group=group,fill=Value.Rate),color='black',alpha=.8,size=.3)+
+  #scale_fill_gradient2(name="",limits=limits,low="navy", mid="white", high="red")+
+  scale_fill_gradient(low="#FFFFFF",high = "#8b0000",limits = c(0,6),
+                      breaks = c(0,3,6))+
+  coord_map()+
+  theme_nothing(base_size=12, legend=T)+
+  ggtitle("Hispanic Rate Map 2010")+theme(plot.title = element_text(hjust = 0.5,size = rel(2.25)),legend.text=element_text(size=rel(2)),legend.key.size=unit(2,"line"))
+Hispplot2010
+
+Hispplot2015<-ggplot()+
+  geom_polygon(data=Hispmapping2015,aes(x=long,y=lat,group=group,fill=Value.Rate),color='black',alpha=.8,size=.3)+
+  #scale_fill_gradient2(name="",limits=limits,low="navy", mid="white", high="red")+
+  scale_fill_gradient(low="#FFFFFF",high = "#8b0000",limits = c(0,6),
+                      breaks = c(0,3,6))+
+  coord_map()+
+  theme_nothing(base_size=12, legend=T)+
+  ggtitle("Hispanic Rate Map 2015")+theme(plot.title = element_text(hjust = 0.5,size = rel(2.25)),legend.text=element_text(size=rel(2)),legend.key.size=unit(2,"line"))
+Hispplot2015
+
+Hispplot2020<-ggplot()+
+  geom_polygon(data=Hispmapping2020,aes(x=long,y=lat,group=group,fill=Value.Rate),color='black',alpha=.8,size=.3)+
+  #scale_fill_gradient2(name="",limits=limits,low="navy", mid="white", high="red")+
+  scale_fill_gradient(low="#FFFFFF",high = "#8b0000",limits = c(0,6),
+                      breaks = c(0,3,6))+
+  coord_map()+
+  theme_nothing(base_size=12, legend=T)+
+  ggtitle("Hispanic Rate Map 2020")+theme(plot.title = element_text(hjust = 0.5,size = rel(2.25)),legend.text=element_text(size=rel(2)),legend.key.size=unit(2,"line"))
+Hispplot2020
+
+grid.arrange(Hispplot2010,Hispplot2015,Hispplot2020,nrow=2,ncol=2)
 
 ##Black Data
 
