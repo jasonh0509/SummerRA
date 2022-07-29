bfadcdc0a329e456ed71a89da26c76edf5c0752a
jasonh0509 <jueshenhou@gmail.com>
2022-07-29 15:11:18 -0400

    add black rate

--- static graph minority.R
+++ static graph minority.R
@@ -199,6 +199,52 @@ Black2010<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/
 Black2015<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/AA2015.csv")
 Black2020<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/AA2020.csv")
 
+totaldatablk<-rbind(Black2010,Black2015,Black2020)
+#Rate.de<-subset(totaldataAsian,select = Rate.Denom)
+totaldatablk<-totaldatablk%>%
+  mutate(Rate.Denom=gsub(",","",Rate.Denom),
+         Rate.Denom=as.numeric(Rate.Denom),
+         Value.Rate=log(Value.Rate))
+
+graphtotalblk<-subset(totaldatablk,select = c(Place,Year,Geoid,Rate.Denom,Value.Count,Value.Rate))
+graphtotalblk$FIPS=graphtotalblk$Geoid-37000
+
+ncMap.mergeblk<-merge(ncMap,graphtotalblk,by.x="id2",by.y="FIPS")
+
+Blkmapping2010<-ncMap.mergeblk[which(ncMap.mergeblk$Year==2010),]
+Blkpmapping2015<-ncMap.mergeblk[which(ncMap.mergeblk$Year==2015),]
+Blkpmapping2020<-ncMap.mergeblk[which(ncMap.mergeblk$Year==2020),]
+
+Blackplot2010<-ggplot()+
+  geom_polygon(data=Blkmapping2010,aes(x=long,y=lat,group=group,fill=Value.Rate),color='black',alpha=.8,size=.3)+
+  #scale_fill_gradient2(name="",limits=limits,low="navy", mid="white", high="red")+
+  scale_fill_gradient(low="#FFFFFF",high = "#8b0000",limits = c(0,6),
+                      breaks = c(0,3,6))+
+  coord_map()+
+  theme_nothing(base_size=12, legend=T)+
+  ggtitle("Black Rate Map 2010")+theme(plot.title = element_text(hjust = 0.5,size = rel(2.25)),legend.text=element_text(size=rel(2)),legend.key.size=unit(2,"line"))
+Blackplot2010
+
+Blackplot2015<-ggplot()+
+  geom_polygon(data=Blkpmapping2015,aes(x=long,y=lat,group=group,fill=Value.Rate),color='black',alpha=.8,size=.3)+
+  #scale_fill_gradient2(name="",limits=limits,low="navy", mid="white", high="red")+
+  scale_fill_gradient(low="#FFFFFF",high = "#8b0000",limits = c(0,6),
+                      breaks = c(0,3,6))+
+  coord_map()+
+  theme_nothing(base_size=12, legend=T)+
+  ggtitle("Black Rate Map 2015")+theme(plot.title = element_text(hjust = 0.5,size = rel(2.25)),legend.text=element_text(size=rel(2)),legend.key.size=unit(2,"line"))
+Blackplot2015
+
+Blackplot2020<-ggplot()+
+  geom_polygon(data=Blkpmapping2020,aes(x=long,y=lat,group=group,fill=Value.Rate),color='black',alpha=.8,size=.3)+
+  #scale_fill_gradient2(name="",limits=limits,low="navy", mid="white", high="red")+
+  scale_fill_gradient(low="#FFFFFF",high = "#8b0000",limits = c(0,6),
+                      breaks = c(0,3,6))+
+  coord_map()+
+  theme_nothing(base_size=12, legend=T)+
+  ggtitle("Black Rate Map 2020")+theme(plot.title = element_text(hjust = 0.5,size = rel(2.25)),legend.text=element_text(size=rel(2)),legend.key.size=unit(2,"line"))
+Blackplot2020
+
 ##White Data
 
 White2010<-read.csv("https://raw.githubusercontent.com/jasonh0509/SummerRA/main/White2010.csv")
